9ba86d5665d425c721cbba9f437df597d09d4252
Branislav Hraško <52451064+BraniH@users.noreply.github.com>
2021-06-10 16:33:16 +0200

    All files
    full program

---
+++ MouseController.py
@@ -0,0 +1,29 @@
+from pynput.mouse import Controller
+import time
+import os
+
+
+def get_current_position():
+    current_path = os.getcwd()
+    return current_path
+
+
+def get_settings():
+    with open(get_current_position() + "\MoveSettings.txt") as config:
+        for line in config:
+            if "time" in line:
+                time_set = line.strip(" ").split("=")[1]
+                return int(time_set)
+
+
+def mouse_movement():
+    mouse = Controller()
+    mouse.move(1, 0)
+
+
+if __name__ == "__main__":
+    while True:
+        mouse_movement()
+        time.sleep(get_settings())
+
+
